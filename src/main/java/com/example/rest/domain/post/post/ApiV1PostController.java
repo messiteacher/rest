995f4d600a55fdd773cf4ddbaa6ffca6e6491c6e
ShinYounHo <suh2963@gmail.com>
2025-01-27 09:25:46 +0900

    modify

--- src/main/java/com/example/rest/domain/post/post/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/ApiV1PostController.java
@@ -2,6 +2,8 @@ package com.example.rest.domain.post.post;
 
 import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.service.PostService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +33,27 @@ public class ApiV1PostController {
         Map<String, Object> rsData = new HashMap<>();
         rsData.put("code", "200-1");
         rsData.put("msg", "%d번 글 삭제가 완료되었습니다.".formatted(id));
+
+        return rsData;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ModifyForm {
+
+        public String title;
+        public String content;
+    }
+
+    @PutMapping("/{id}")
+    public Map<String, Object> modify(@PathVariable long id, @RequestBody ModifyForm form) {
+
+        Post post = postService.getItem(id).get();
+        postService.modify(post, form.getTitle(), form.getContent());
+
+        Map<String, Object> rsData = new HashMap<>();
+        rsData.put("code", "200-1");
+        rsData.put("msg", "%d번 글 수정이 완료되었습니다.".formatted(id));
 
         return rsData;
     }
