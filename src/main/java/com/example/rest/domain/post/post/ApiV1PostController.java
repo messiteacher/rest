6a884d2bdf914cd3d3be15d3e02d8321a4e38001
ShinYounHo <suh2963@gmail.com>
2025-01-27 09:39:37 +0900

    RsData apply

--- src/main/java/com/example/rest/domain/post/post/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/ApiV1PostController.java
@@ -2,6 +2,7 @@ package com.example.rest.domain.post.post;
 
 import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.service.PostService;
+import com.example.rest.global.dto.RsData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -25,16 +26,14 @@ public class ApiV1PostController {
     }
 
     @DeleteMapping("/{id}")
-    public Map<String, Object> delete(@PathVariable long id) {
+    public RsData delete(@PathVariable long id) {
 
         Post post = postService.getItem(id).get();
         postService.delete(post);
 
-        Map<String, Object> rsData = new HashMap<>();
-        rsData.put("code", "200-1");
-        rsData.put("msg", "%d번 글 삭제가 완료되었습니다.".formatted(id));
-
-        return rsData;
+        return new RsData(
+                "200-1",
+                "%d번 글 삭제가 완료되었습니다.".formatted(id));
     }
 
     @AllArgsConstructor
@@ -46,15 +45,13 @@ public class ApiV1PostController {
     }
 
     @PutMapping("/{id}")
-    public Map<String, Object> modify(@PathVariable long id, @RequestBody ModifyForm form) {
+    public RsData modify(@PathVariable long id, @RequestBody ModifyForm form) {
 
         Post post = postService.getItem(id).get();
         postService.modify(post, form.getTitle(), form.getContent());
 
-        Map<String, Object> rsData = new HashMap<>();
-        rsData.put("code", "200-1");
-        rsData.put("msg", "%d번 글 수정이 완료되었습니다.".formatted(id));
-
-        return rsData;
+        return new RsData(
+                "200-1",
+                "%d번 글 수정이 완료되었습니다.".formatted(id));
     }
 }
