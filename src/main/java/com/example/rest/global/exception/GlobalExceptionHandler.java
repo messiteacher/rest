1b91085d915e9955d93b39a916c85688c6244b19
ShinYounHo <suh2963@gmail.com>
2025-01-31 14:00:21 +0900

    DataIntegrityEx

--- src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.rest.global.exception;
 
 import com.example.rest.global.app.AppConfig;
 import com.example.rest.global.dto.RsData;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -27,20 +28,33 @@ public class GlobalExceptionHandler {
         ));
     }
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-//
-//        String message = e.getBindingResult().getFieldErrors()
-//                .stream()
-//                .map(fe -> fe.getField() + " : " + fe.getCode() + " : " + fe.getDefaultMessage())
-//                .sorted()
-//                .collect(Collectors.joining("\n"));
-//
-//        return ResponseEntity
-//                .status(HttpStatus.NOT_FOUND)
-//                .body(new RsData<>(
-//                        "400-1",
-//                        message
-//                ));
-//    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        String message = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fe -> fe.getField() + " : " + fe.getCode() + " : " + fe.getDefaultMessage())
+                .sorted()
+                .collect(Collectors.joining("\n"));
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new RsData<>(
+                        "400-1",
+                        message
+                ));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<RsData<Void>> DataIntegrityViolationExceptionHandle(DataIntegrityViolationException e) {
+
+        if (AppConfig.isNotProd()) e.printStackTrace();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new RsData<>(
+                        "404-1",
+                        "이미 존재하는 데이터입니다."
+                ));
+    }
 }
